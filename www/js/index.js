c9a8364ca0707e447338a721bc666d35bfa4cb5c
Michael Walters <waltersm8@winthrop.edu>
2021-02-25 13:02:09 -0500

    format update

--- www/js/index.js
+++ www/js/index.js
@@ -39,14 +39,15 @@ function getPosition() {
     var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
  
     function onSuccess(position) {
-       alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+       alert(
+            'Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
     };
  
     function onError(error) {
