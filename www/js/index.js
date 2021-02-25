75ce9ce7d2420e4ebd60e385c2cd72f4f2b92bd9
Michael Walters <waltersm8@winthrop.edu>
2021-02-25 13:01:34 -0500

    Added geolocation functions

--- www/js/index.js
+++ www/js/index.js
@@ -26,4 +26,54 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+
+    document.getElementById("getPosition").addEventListener("click", getPosition);
+    document.getElementById("watchPosition").addEventListener("click", watchPosition);
 }
+
+function getPosition() {
+    var options = {
+       enableHighAccuracy: true,
+       maximumAge: 3600000
+    }
+    var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
+ 
+    function onSuccess(position) {
+       alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    };
+ 
+    function onError(error) {
+       alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+    }
+ }
+ 
+ function watchPosition() {
+    var options = {
+       maximumAge: 3600000,
+       timeout: 3000,
+       enableHighAccuracy: true,
+    }
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+ 
+    function onSuccess(position) {
+       alert('Latitude: '          + position.coords.latitude          + '\n' +
+          'Longitude: '         + position.coords.longitude         + '\n' +
+          'Altitude: '          + position.coords.altitude          + '\n' +
+          'Accuracy: '          + position.coords.accuracy          + '\n' +
+          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          'Heading: '           + position.coords.heading           + '\n' +
+          'Speed: '             + position.coords.speed             + '\n' +
+          'Timestamp: '         + position.timestamp                + '\n');
+    };
+ 
+    function onError(error) {
+       alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
+    }
+ }
