10e39e08eb59fc945da9489a5602f19ea0e5b9f5
Michael Walters <waltersm8@winthrop.edu>
2021-03-02 11:55:23 -0500

    Should be displaying current speed

--- www/js/index.js
+++ www/js/index.js
@@ -58,21 +58,29 @@ function getPosition() {
  function watchPosition() {
     var options = {
        maximumAge: 3600000,
-       timeout: 3000,
+       timeout: 250,
        enableHighAccuracy: true,
     }
     var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
  
     function onSuccess(position) {
-       alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-    };
+       /*
+         alert('Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n'
+         );
+            */
+
+         var speedDisplay = document.getElementById('currentSpeed');
+
+         speedDisplay.innerHTML = position.coords.speed;
+      };
+    
  
     function onError(error) {
        alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
