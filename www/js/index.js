e29a2ffc8a68546bd532d256ae0599bd00bc9eb8
Michael Walters <waltersm8@winthrop.edu>
2021-03-04 12:49:41 -0500

    Some nice comments explaining whats happening a little

--- www/js/index.js
+++ www/js/index.js
@@ -1,37 +1,17 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+document.addEventListener('deviceready', onDeviceReady, false); //When device ready call onDeviceReady
+document.addEventListener("DOMContentLoaded", onPageReady, false); //When page loaded call onPageReady
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    //document.getElementById('deviceready').classList.add('ready');
-
-    document.getElementById("getPosition").addEventListener("click", getPosition);
-    document.getElementById("watchPosition").addEventListener("click", watchPosition);
+function onDeviceReady() { //Device ready
+   console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
 }
 
-function getPosition() {
+function onPageReady() { //Page ready
+   document.getElementById("getPosition").addEventListener("click", getPosition); //Click button to get current position
+   
+   watchPosition(); //Tracks via gps 
+}
+
+function getPosition() { //This is more of a debugging function to run when get position button is clicked
     var options = {
        enableHighAccuracy: true,
        maximumAge: 3600000
@@ -51,8 +31,8 @@ function getPosition() {
     };
  
     function onError(error) {
-       alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
-    }
+      alert('Oopsies the app developers suck \n\n The app should still run just fine we promise \n\n code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
+   }
  }
  
  function watchPosition() {
@@ -61,31 +41,19 @@ function getPosition() {
        timeout: 200,
        enableHighAccuracy: true,
     }
-    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, options); //Gets the data 
  
-    function onSuccess(position) {
-         /*
-         alert('Latitude: '          + position.coords.latitude          + '\n' +
-            'Longitude: '         + position.coords.longitude         + '\n' +
-            'Altitude: '          + position.coords.altitude          + '\n' +
-            'Accuracy: '          + position.coords.accuracy          + '\n' +
-            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-            'Heading: '           + position.coords.heading           + '\n' +
-            'Speed: '             + position.coords.speed             + '\n' +
-            'Timestamp: '         + position.timestamp                + '\n'
-         );
-            */
-
-         var speedDisplay = document.getElementById('currentSpeed');
+    function onSuccess(position) { //When data get is successfull
+         var speedDisplay = document.getElementById('currentSpeed'); //Get current speed element
 
          speed = position.coords.speed;
          speed = speed.toString();
 
-         speedDisplay.innerHTML = speed.split('.')[0];
+         speedDisplay.innerHTML = speed.split('.')[0]; //Set current speed on the app
       };
     
  
     function onError(error) {
-       alert('code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
+       alert('Oopsies the app developers suck \n\n The app should still run just fine we promise \n\n code: '    + error.code    + '\n' +'message: ' + error.message + '\n');
     }
  }
